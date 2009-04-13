57f7ea45bfa65e6830f9843c80e496b48ac36b62
slnc <s@slnc.me>
2009-04-13 14:56:17 +0200

    corregidos errores de carga de librerías que causaban que fallasen algunas cosas

--- rails_mixings/lib/rails_mixings.rb
+++ rails_mixings/lib/rails_mixings.rb
@@ -2,13 +2,33 @@ if File.exists?("#{RAILS_ROOT}/config/initializers/pre_plugins.rb")
   require "#{RAILS_ROOT}/config/initializers/pre_plugins.rb"
 end
 
-$:.unshift "#{File.dirname(__FILE__)}/lib"
-
+require 'active_record_mixings.rb'
 ActiveRecord::Base.send :include, ActiveRecordMixings
+
+require 'action_view_mixings.rb'
 ActionView::Base.send :include, ActionViewMixings
+
+require 'action_controller_mixings.rb'
 ActionController::Base.send :include, ActionControllerMixings
+
+require 'user_mixings.rb'
 User.send :include, UserMixings
+
+%w(
+ab_test  
+action_mailer
+friendship
+geolocation
+notification_mixings
+silenced_email
+stats_mixings  
+).each do |f|
+  require "#{File.dirname(__FILE__)}/#{f}.rb"
+end
+puts "after requires"
+# $:.unshift "#{File.dirname(__FILE__)}/lib"
+
 Notification.send :include, NotificationMixings
 Stats::Goals.send :include, StatsMixings::GoalsMixings
 
-require 'test_unit_mixings'
+# require 'test_unit_mixings'
