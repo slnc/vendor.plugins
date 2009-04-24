b96cfc78bea5209f97442dd8fcf0ea83562b6f32
slnc <s@slnc.me>
2009-04-24 22:54:22 +0200

    quitado código de assert_cookie ya que no lo usamos

--- rails_mixings/lib/test_unit_mixings.rb
+++ rails_mixings/lib/test_unit_mixings.rb
@@ -130,50 +130,6 @@ module Test::Unit::Assertions
     end
   end
   
-  # Custom assertions for cookies
-  #
-  #   assert_cookie :pass, 
-  #     :value => lambda { |value| UUID.parse(value).valid? }
-  #
-  #   assert_cookie :yellow, :value => ['sunny', 'days']
-  #
-  #   assert_cookie :delight, :value => 'yum'
-  #
-  #   assert_cookie :secret, :path => lambda { |path| path =~ /secret/ }, 
-  #     :secure => true   
-  def assert_cookie(name, options={}, message="")
-    clean_backtrace do
-      cookie = cookies[name.to_s]
-      
-      msg = build_message(message, "expected cookie named <?> but it was not found.", name)
-      assert_not_nil cookie, msg
-      
-      case 
-        when options[:value].respond_to?(:call)
-        msg = build_message(message,
-                    "expected result of value block to be true but it was false.")
-        cookie.value.each do |value|
-          assert(options[:value].call(value), msg)
-        end
-        when options[:value].respond_to?(:each)
-        options[:value].each do |value|
-          msg = build_message(message, 
-                      "expected cookie value to include <?> but it was not found.", value)
-          assert(cookie.value.include?(value), msg)
-        end
-      else
-        msg = build_message(message, "expected cookie value to be <?> but it was <?>.",
-        options[:value], cookie.value)
-        assert(cookie.value.include?(options[:value]), msg)
-      end if options.key?(:value)
-      
-      assert_call_or_value :path, options, cookie, message
-      assert_call_or_value :domain, options, cookie, message
-      assert_call_or_value :expires, options, cookie, message
-      assert_call_or_value :secure, options, cookie, message
-    end
-  end
-  
   # Tests that a cookie named +name+ does not exist. This is useful
   # because cookies['name'] may be nil or [] in a functional test.
   #
