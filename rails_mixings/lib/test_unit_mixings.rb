fb4fa5215f23e9960123b4de81b644009c89c049
slnc <s@slnc.me>
2009-04-24 21:44:00 +0200

    añadido workaround para fixture_file_upload no existente en TestCase

--- rails_mixings/lib/test_unit_mixings.rb
+++ rails_mixings/lib/test_unit_mixings.rb
@@ -1,5 +1,10 @@
 require 'test/unit'
 module Test::Unit::Assertions
+  def fixture_file_upload(path, mime_type = nil, binary = false)
+    fixture_path = ActionController::TestCase.send(:fixture_path) if ActionController::TestCase.respond_to?(:fixture_path)
+    ActionController::TestUploadedFile.new("#{fixture_path}#{path}", mime_type, binary)
+  end
+  
   def overload_rake_for_tests
     load File.dirname(__FILE__) + '/overload_rake_for_tests.rb'
   end
