2ae932f5d8134fa2b4d033251c67fabd9c46028c
slnc <s@slnc.me>
2009-06-16 12:01:48 +0200

    usar un return_path siempre

--- rails_mixings/lib/action_mailer.rb
+++ rails_mixings/lib/action_mailer.rb
@@ -1,6 +1,7 @@
 # necesario para que el Return-Path se ponga bien.
 class ActionMailer::Base
   def perform_delivery_sendmail(mail)
+    @return_path = App.system_mail_user if @return_path.nil?
     IO.popen("#{sendmail_settings[:location]} -f #{mail.from} -r #{@return_path} #{sendmail_settings[:arguments]}","w+") do |sm|
       sm.print(mail.encoded.gsub(/\r/, ''))
       sm.flush
