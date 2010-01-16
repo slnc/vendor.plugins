fa16d870a7dc9ad6a6879e4aa4e2703996b836c8
slnc <s@slnc.me>
2010-01-15 18:37:55 -0600

    disable geolocation if necessary

--- rails_mixings/test/geolocation_test.rb
+++ rails_mixings/test/geolocation_test.rb
@@ -1,16 +1,18 @@
 require File.dirname(__FILE__) + '/../../../../test/test_helper'
 require 'RMagick'
 
-class GeolocationTest < ActiveSupport::TestCase
-  test "should_properly_resolve_known_ip" do
-    assert_equal 'es', Geolocation.country_code_by_addr('87.217.161.31')
-  end
-  
-  test "resolve_ad_mode_should_properly_return_es_for_es_ip" do
-    assert_equal 'es', Geolocation.resolve_ad_mode('87.217.161.31')
-  end
-  
-  test "resolve_ad_mode_should_properly_return_sa_for_ve_ip" do
-    assert_equal 'sa', Geolocation.resolve_ad_mode('150.188.229.4')
+unless Geolocation::DISABLED
+  class GeolocationTest < ActiveSupport::TestCase
+    test "should_properly_resolve_known_ip" do
+      assert_equal 'es', Geolocation.country_code_by_addr('87.217.161.31')
+    end
+    
+    test "resolve_ad_mode_should_properly_return_es_for_es_ip" do
+      assert_equal 'es', Geolocation.resolve_ad_mode('87.217.161.31')
+    end
+    
+    test "resolve_ad_mode_should_properly_return_sa_for_ve_ip" do
+      assert_equal 'sa', Geolocation.resolve_ad_mode('150.188.229.4')
+    end
   end
 end
